6e256ba3024a0739ad0b66853dd8756aed759475
omar-z-dev <zaoudioo@gmail.com>
2026-08-11 23:33:14 +0200

    jj

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -7,14 +7,20 @@ use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusExce
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
+
+
+/*une classe que le composant Security appelle automatiquement pendant la connexion.*/
 class UserChecker implements UserCheckerInterface
 {
     public function checkPreAuth(UserInterface $user): void
     {
+        //si l’objet reçu n’est pas une instance de l'entité App\Entity\User, on arrête cette vérification.
         if (!$user instanceof User) {
             return;
         }
 
+        //si l’utilisateur est banni on arrête la connexion et on affiche un message
         if ($user->isBanned()) {
             throw new CustomUserMessageAccountStatusException(
                 'Vous êtes banni. Vous ne pouvez pas vous connecter, veuillez contacter un administrateur.'
@@ -22,6 +28,7 @@ class UserChecker implements UserCheckerInterface
         }
     }
 
+    //vérification n’est utile que si  mis en place la confirmation du compte par email. 
     public function checkPostAuth(UserInterface $user): void
     {
     }
